98474d05ded326eec858e8589ecc34990f7f7705
sik2 <bickwang123@gmail.com>
2024-10-15 10:24:19 +0900

    9

--- src/main/java/com/market/apple/domain/member/controller/MemberController.java
+++ src/main/java/com/market/apple/domain/member/controller/MemberController.java
@@ -3,10 +3,17 @@ package com.market.apple.domain.member.controller;
 import com.market.apple.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
+@RequestMapping("/member")
 @Controller
 public class MemberController {
     private final MemberService memberService;
 
+    @GetMapping("/login")
+    public String loginPage() {
+        return "member/login";
+    }
 }
