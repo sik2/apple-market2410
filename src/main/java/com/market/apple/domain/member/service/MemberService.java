395b9ccf1ef89e962bfea86c3359cf65528c4e32
sik2 <bickwang123@gmail.com>
2024-10-15 11:49:33 +0900

    13

--- src/main/java/com/market/apple/domain/member/service/MemberService.java
+++ src/main/java/com/market/apple/domain/member/service/MemberService.java
@@ -5,6 +5,9 @@ import com.market.apple.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,4 +26,17 @@ public class MemberService {
         return  this.memberRepository.save(member);
     }
 
+    @Transactional
+    public Member whenSocialLogin(String providerTypeCode, String username, String nickname) {
+        Optional<Member> opMember = findByUsername(username);
+
+        if (opMember.isPresent()) return opMember.get();
+
+        // 소셜 로그인를 통한 가입시 비번은 없다.
+        return join(username, "", nickname, ""); // 최초 로그인 시 딱 한번 실행
+    }
+
+    private Optional<Member> findByUsername(String username) {
+        return memberRepository.findByusername(username);
+    }
 }
